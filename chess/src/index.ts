63e3464a3f9ac57803425bf9a14ec960e8f74a0f
akash23-ai <akashbhardwaj2024@gmail.com>
2024-07-20 22:39:04 +0530

    backend state management

---
+++ chess/src/index.ts
@@ -0,0 +1,9 @@
+import { gameManager } from "./store";
+import { startLogger } from "./logger"
+
+
+startLogger();
+
+setInterval(() => {
+    gameManager.addGame(Math.random().toString());
+}, 5000)
